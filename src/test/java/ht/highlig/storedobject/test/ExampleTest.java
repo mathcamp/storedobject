71444b568bf61d3f70ebce5cfc5a86c5c0bd9c47
Revant <revant.kapoor@gmail.com>
2014-06-24 13:14:55 -0700

    Wireframe for unit testing

---
+++ src/test/java/ht/highlig/storedobject/test/ExampleTest.java
@@ -0,0 +1,19 @@
+package ht.highlig.storedobject.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by revant on 6/24/14.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+public class ExampleTest {
+
+    @Test
+    public void testBlah() {
+        assert true;
+    }
+}
